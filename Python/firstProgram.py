c3a4037faaa387a14058f340c8891fe8405b2796
aviralvermamca2025-spec <aviral.verma.mca.2025@miet.ac.in>
2026-02-18 22:54:58 +0530

    adding print

--- Python/firstProgram.py
+++ Python/firstProgram.py
@@ -1,13 +1,13 @@
 # this is the first program
-# print("Hello this is the first program")
+print("first Program")
 # --- Simple Interest Calculator ---
 
 # name = input("Enter your name : ")
-principal = float(input("Enter the rate of interest(in rs)"))
-rate = float(input("Enter the rate of interest (in %)"))
-time = int (input("Enter the time(in years): "))
+# principal = float(input("Enter the rate of interest(in rs)"))
+# rate = float(input("Enter the rate of interest (in %)"))
+# time = int (input("Enter the time(in years): "))
 
-print("--------------------------------------------")
-simple_interest = (principal * rate * time) /100
+# print("--------------------------------------------")
+# simple_interest = (principal * rate * time) /100
 
-print()
+# print()
